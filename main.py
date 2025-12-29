8c6761b238110e3d374670f41bd4eea81dfd9455
juanfrilla <juanframaro@gmail.com>
2025-12-29 07:24:56 +0000

    leave as before

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from dateutil.parser import isoparse
 
 from constants import DATA_FILE, LOG_DIR
 from logger import get_logger
-# from scrapers.empleate import EmpleateScraper
-# from scrapers.infojobs import InfoJobsScraper
-# from scrapers.linkedin import LinkedinScraper
+from scrapers.empleate import EmpleateScraper
+from scrapers.infojobs import InfoJobsScraper
+from scrapers.linkedin import LinkedinScraper
 from scrapers.simplyhired import SimplyHiredScraper
 from utils import (
     last_scraped_today,
@@ -20,8 +20,7 @@ from utils import (
 
 
 def scrape_everything():
-    #scrapers = [LinkedinScraper, InfoJobsScraper, EmpleateScraper]  # SimplyHiredScraper
-    scrapers = [SimplyHiredScraper]
+    scrapers = [LinkedinScraper, InfoJobsScraper, EmpleateScraper]  # SimplyHiredScraper
     job_posts = []
     for ScraperClass in scrapers:
         scraper = ScraperClass()
